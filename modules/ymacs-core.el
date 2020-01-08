426acfc8caa295bdc821a210221e8ba37fb230aa
Yu Yi <yiyu@google.com>
2020-01-08 15:54:56 -0500

    add mwim mode

--- modules/ymacs-core.el
+++ modules/ymacs-core.el
@@ -221,4 +221,11 @@
   (global-set-key [f8] 'neotree-toggle)
   )
 
+;; mwim moves smartly
+(use-package mwim
+  :bind
+  ("C-a" . mwim-beginning-of-code-or-line)
+  ("C-e" . mwim-end-of-code-or-line))
+
+
 (provide 'ymacs-core)
