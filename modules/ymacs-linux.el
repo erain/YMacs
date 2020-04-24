ee2c1bd2edb916846907f0a2e4e12be2137725dc
Yu Yi <yiyu@google.com>
2020-04-24 12:27:18 -0400

    Fix the exec-path-from-shell problem
    - The real problem was errors in the .zshrc file

--- modules/ymacs-linux.el
+++ modules/ymacs-linux.el
@@ -1,6 +1,11 @@
 ;;; ymacs-linux.el --- linux specific settings.
 
-;; (use-package exec-path-from-shell)
-;; (exec-path-from-shell-initialize)
+(use-package exec-path-from-shell
+  :ensure t
+  :init
+  ;; (setq exec-path-from-shell-debug t)
+  (message "Should be running exec-path-from-shell-initialize")
+  (exec-path-from-shell-initialize)
+  )
 
 (provide 'ymacs-linux)
