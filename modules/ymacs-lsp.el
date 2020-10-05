b00b47fdf878d979703221718190db9707661455
Yu Yi <yiyu@google.com>
2020-10-05 13:25:09 +0000

    Setup yasnipet for c-mode

--- modules/ymacs-lsp.el
+++ modules/ymacs-lsp.el
@@ -18,7 +18,8 @@
   (use-package yasnippet-snippets       ; Collection of snippets
     :ensure t)
   :commands yas-minor-mode
-  :hook (go-mode . yas-minor-mode)
+  :hook ((go-mode . yas-minor-mode)
+	 (c-mode . yas-minor-mode))
   )
 
 
