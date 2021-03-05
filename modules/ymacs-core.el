57ff5f45fee8c909595c995736a36eead91b14d5
Yu Yi <yiyu@axelar.network>
2021-03-04 21:26:44 -0500

    change some keybindings for treemacs

--- modules/ymacs-core.el
+++ modules/ymacs-core.el
@@ -313,10 +313,10 @@
   (:map global-map
 	("M-0"       . treemacs-select-window)
 	("C-x t 1"   . treemacs-delete-other-windows)
-	("C-x t t"   . treemacs)
-	("C-x t B"   . treemacs-bookmark)
-	("C-x t C-t" . treemacs-find-file)
-	("C-x t M-t" . treemacs-find-tag)))
+	("C-t"   . treemacs)
+	("C-x t b"   . treemacs-bookmark)
+	("C-x t f" . treemacs-find-file)
+	("C-x t t" . treemacs-find-tag)))
 
 (use-package treemacs-projectile
   :after treemacs projectile
